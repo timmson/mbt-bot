5b7243369a880add6a1c43fd821535b963f64ca9
timmson <timmson666@mail.ru>
2016-12-19 23:30:01 +0300

    Fix about

--- routes/about-route.js
+++ routes/about-route.js
@@ -2,7 +2,6 @@ var packageInfo = require('../package.json');
 
 module.exports = {
     handle: function (ctx, message, sendMessage) {
-
-        sendMessage(ctx, message.from, packageInfo.name + ' ' +  packageInfo.version + '\nhttps://github.com/timmson/node');
+        sendMessage(ctx, message.from, packageInfo.name + ' ' +  packageInfo.version + '\n' + packageInfo.repository.url);
     }
 };
