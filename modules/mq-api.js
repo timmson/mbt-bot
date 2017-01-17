3a5e22a5d918a5ea20a6ae60ef82cd0c4692dd13
timmson <timmson666@mail.ru>
2017-01-18 00:07:58 +0300

    #10

--- modules/mq-api.js
+++ modules/mq-api.js
@@ -15,7 +15,7 @@ MqApi.prototype.start = function () {
     });
 
     connection.addListener('ready', () => {
-        connection.queue('bot.queue.in', {}, queue => {
+        connection.queue('bot.queue.in', {'exclusive': false}, queue => {
             queue.bind(_ctx.config.mq.exchange, '#');
             queue.subscribe(message => {
                 _ctx.log.info("Message received " + message);
