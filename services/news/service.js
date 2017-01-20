342a681d96b3c46b8b512ab86534c172e056c3e3
timmson <timmson666@mail.ru>
2017-01-20 21:34:42 +0300

    Add news servie

--- services/news/service.js
+++ services/news/service.js
@@ -7,24 +7,28 @@ const CronJob = require('cron').CronJob;
 var lastStared = new Date();
 console.log("Service started");
 
-/*config.topic.forEach(topic => {
- new CronJob(config.cron,
- () =>getNewFeeds(topic),
- () => {
- },
- true
- );
- });*/
+config.topic.forEach(getNewFeeds/*schedule*/);
 
-config.topic.forEach(getNewFeeds);
+function schedule(topic) {
+    new CronJob(config.cron,
+        () =>getNewFeeds(topic),
+        () => {
+        },
+        true
+    );
+}
+
 
 function getNewFeeds(topic) {
     console.log("Update topic " + topic.name);
-    main.getFeeds(topic, (err, feeds) => err ? console.error(err.stack) : feeds.filter(isNew).forEach(feed => sendMessage(topic.channel, feed.link)));
+    main.getFeeds(topic, (err, feeds) =>
+        err ? console.error(err.stack) : feeds.filter(isNew).forEach(feed => sendMessage(topic.channel, feed.link))
+    );
 }
 
 function isNew(feed) {
     //return feed.pubDate >= lastStared;
+    lastStared = new Date();
     return true;
 }
 
@@ -32,11 +36,11 @@ function sendMessage(to, message) {
     console.info(to + " <- " + message);
     const connection = AMQP.createConnection(config.mq.connection);
 
-    connection.on('error', error => console.error("Error from amqp: " + error.stack));
+    connection.on('error', err => console.error("Error from amqp: " + err.stack));
 
     connection.on('ready', () => {
-        connection.exchange(config.mq.exchange, {type: 'fanout', durable: true, autoDelete: false}, exchange => {
+        connection.exchange(config.mq.exchange, {type: 'fanout', durable: true, autoDelete: false}, exchange =>
             exchange.publish('', message, {}, (isSend, err) => err ? console.error(err.stack) : 0)
-        });
+        );
     });
 }
