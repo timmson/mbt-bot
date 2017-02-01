0eaa601ed8f0088f494f3fe2e9d146846938b5c4
KROTOV Artem <artem.krotov@raiffeisen.ru>
2017-02-01 16:42:56 +0300

    Add hostSvc

--- routes/camera-pc-route.js
+++ routes/camera-pc-route.js
@@ -6,6 +6,11 @@ module.exports = {
         const imageUrl = 'http://' + hostSvc.host + ':' + hostSvc.port + '/camera.jpg';
         const fileName = '/tmp/' + imageUrl.split('/').pop();
         ctx.log.debug('Downloading ' + imageUrl + ' -> ' + fileName);
-        ctx.request(imageUrl).pipe(fs.createWriteStream(fileName)).on('close', () => ctx.bot.sendPhoto(message.from, fileName, {}));
+        try {
+            ctx.request(imageUrl).pipe(fs.createWriteStream(fileName)).on('close', () => ctx.bot.sendPhoto(message.from, fileName, {}));
+        } catch (err) {
+            ctx.log.error(err);
+            ctx.bot.sendMessage(message.from, 'Service is unavailable', {});
+        }
     }
 };
--- routes/screen-pc-route.js
+++ routes/screen-pc-route.js
@@ -6,7 +6,12 @@ module.exports = {
         const imageUrl = 'http://' + hostSvc.host + ':' + hostSvc.port + '/screen.jpg';
         const fileName = '/tmp/' + imageUrl.split('/').pop();
         ctx.log.debug('Downloading ' + imageUrl + ' -> ' + fileName);
-        ctx.request(imageUrl).pipe(fs.createWriteStream(fileName)).on('close', () => ctx.bot.sendPhoto(message.from, fileName, {}));
+        try {
+            ctx.request(imageUrl).pipe(fs.createWriteStream(fileName)).on('close', () => ctx.bot.sendPhoto(message.from, fileName, {}));
+        } catch (err) {
+            ctx.log.error(err);
+            ctx.bot.sendMessage(message.from, 'Service is unavailable', {});
+        }
     }
 };
 
