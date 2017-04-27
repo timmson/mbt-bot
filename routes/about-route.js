612d726ff1620eb12cd0870397d28f09078f7b9e
timmson <timmson666@mail.ru>
2017-04-27 23:35:01 +0300

    #24

--- routes/about-route.js
+++ routes/about-route.js
@@ -1,7 +1,9 @@
-var packageInfo = require('../package.json');
+let packageInfo = require('../package.json');
 
 module.exports = {
     handle: function (ctx, message, sendMessage) {
-        sendMessage(ctx, message.from, packageInfo.name + ' ' +  packageInfo.version + '\n' + packageInfo.repository.url);
+        ctx.bot.sendMessage(message.from, packageInfo.name + ' ' + packageInfo.version + '\n' + packageInfo.repository.url, {
+            disable_web_page_preview: true,
+        });
     }
 };
