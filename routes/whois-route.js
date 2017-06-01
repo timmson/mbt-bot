8324a3bb39cd95f7f4d38905a3e3d754f163d177
timmson <timmson666@mail.ru>
2017-06-01 22:31:28 +0300

    #26

--- routes/whois-route.js
+++ routes/whois-route.js
@@ -1,8 +1,8 @@
 module.exports = {
     handle: (ctx, message, sendMessage) => ctx.hostSvc.api('net.json', message.from, (err, body, ctx, to) => {
         const data = JSON.parse(body);
-        const text = data.reduce((last, current) => last + '\n' + current.ip + ' ' + (current.description == '?' ? current.description : current.mac), '');
-        sendMessage(ctx, to, text);
+        const text = data.reduce((last, current) => last + '\n' + current.ip + ' ' + (current.description == '?' ? current.description : current.mac));
+        sendMessage(ctx, to, text||'Никого нет:)');
     })
 };
 
