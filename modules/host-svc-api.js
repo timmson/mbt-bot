d69dc6a1b92a617b4804426693c05dd6437f08d8
timmson <timmson666@mail.ru>
2017-06-14 23:01:25 +0300

    #28

--- modules/host-svc-api.js
+++ modules/host-svc-api.js
@@ -16,7 +16,8 @@ HostSvcApi.prototype.downloadPicture = (path, to) => {
     try {
         ctx.request(imageUrl).on('response', (response) => {
             if (response.statusCode != 200) {
-                throw new Error(response.body);
+                ctx.log.error(response.body);
+                ctx.bot.sendMessage(to, response.body, {});
             }
             response.pipe(fs.createWriteStream(fileName)).on('close', () => ctx.bot.sendPhoto(to, fileName, {}));
         });
